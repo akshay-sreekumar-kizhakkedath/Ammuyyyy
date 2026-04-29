2df6869bdfcd4b2c281512e881249ce8fee89c0f
akshay-sreekumar-kizhakkedath <akshaysreekumarkizhakkedath@gmail.com>
2026-04-29 23:16:35 +0530

    Fix GitHub pages routing issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle, { theme } from './styles/globalStyles';
 import CustomCursor from './components/CustomCursor';
